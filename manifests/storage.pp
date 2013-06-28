8f3d7c6cb2f3a41a4182fc370e32cdb4ff1282a3
Igor Galić <i.galic@brainsware.org>
2013-06-28 23:03:33 +0200

    only update udev rules if $storage populated
    populated by /dev/ices, that is.

--- manifests/storage.pp
+++ manifests/storage.pp
@@ -12,24 +12,29 @@ class trafficserver::storage {
     default => $storage_passed,
   }
 
-  Exec {
-    path => '/bin:/usr/bin:/sbin:/usr/sbin',
-    cwd  => '/',
-  }
-
+  # we can take either [ '/dev/sdc', '/dev/sdb'] or
+  # { '/var/cache/ats' => '4GB', } with this augeas template:
   augeas { 'trafficserver.storage':
     lens    => 'Trafficserver_storage.storage_lns',
     context => "/files${sysconfdir}/storage.config",
     incl    => "${sysconfdir}/storage.config",
     changes => template('trafficserver/storage.config.erb'),
   }
-  file { '/etc/udev/rules.d/51-cache-disk.rules':
-    content => template('trafficserver/51-cache-disk.rules.erb'),
-    notify  => Exec['update udev rules'],
-  }
 
-  exec { 'update udev rules':
-    command     => 'udevadm control --start-exec-queue',
-    refreshonly => true,
+  # However, we only want to execute these Udev rules, if we got a non-empty array:
+  if is_array($storage) and $storage {
+    Exec {
+      path => '/bin:/usr/bin:/sbin:/usr/sbin',
+      cwd  => '/',
+    }
+    file { '/etc/udev/rules.d/51-cache-disk.rules':
+      content => template('trafficserver/51-cache-disk.rules.erb'),
+      notify  => Exec['update udev rules'],
+    }
+
+    exec { 'update udev rules':
+      command     => 'udevadm control --start-exec-queue',
+      refreshonly => true,
+    }
   }
 }
