da5f3163e6a0ebcb821f8a4383f7a3613affc668
Igor Galić <i.galic@brainsware.org>
2013-09-04 15:19:56 +0000

    let puppet guess service by itself
    * this makes it more platform independant.
    (also, the latest version of my package adds a symlink between
    /etc/init/trafficserver.conf and /lib/init/upstart-job ;)

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,8 +1,7 @@
+# This class ensures the service is running, and can be notified
+# for config changes
 class trafficserver::service {
-
   service { 'trafficserver':
     ensure   => 'running',
-    provider => 'upstart',
   }
-
 }
