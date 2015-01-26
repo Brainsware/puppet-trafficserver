19753df4030ebc8f349217d9b33cbfa273e3c7bf
Igor Galić <i.galic@brainsware.org>
2015-01-26 07:42:22 +0100

    remove "code duplication" (on catalogue level)

--- manifests/storage/linux.pp
+++ manifests/storage/linux.pp
@@ -10,11 +10,6 @@ define trafficserver::storage::linux (
     ensure  => $ensure,
     target  => $trafficserver::params::device_file,
     content => template($trafficserver::params::device_template),
-  } ~>
-  exec { 'update udev rules':
-    command     => 'udevadm trigger --subsystem-match=block',
-    path        => '/bin:/usr/bin:/sbin:/usr/sbin',
-    cwd         => '/',
-    refreshonly => true,
+    notify  => Exec['update udev rules'],
   }
 }
