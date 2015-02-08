bb3babc91deda4f93a40cc7d6090d2ecaf97b80c
Igor Galić <i.galic@brainsware.org>
2015-02-08 14:59:19 +0100

    start out remap configuration from with concat
    too much pain to use parsed file, so we'll go with this..

---
+++ manifests/config/remap.pp
@@ -0,0 +1,19 @@
+# basic (concat) setup for trafficserver remap
+class trafficserver::config::remap {
+
+  # these variables will also be used in the udev file header
+  $owner = $trafficserver::user
+  $group = $trafficserver::group
+
+  concat { $trafficserver::params::remap_config:
+    owner  => $owner,
+    group  => $group,
+    notify => Exec['trafficserver-config-reload'],
+  }
+
+  concat::fragment { 'storage_header':
+    order   => '00',
+    target  => $trafficserver::params::remap_config,
+    content => template($trafficserver::params::remap_header),
+  }
+}
